afe80207e00ad665cb14455ae6ad2a43435d8b7a
04amanrajj <04.aman.raj@gmail.com>
2024-11-16 16:57:06 +0530

    New: Server setup

---
+++ configs/db.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+exports.dbconnection = mongoose.connect(process.env.MONGO_URI);
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { dbconnection } = require("./configs/db");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 4500;
+
+app.get("/", (req, res) => {
+  res.status(200).send("homepage");
+});
+
+app.listen(port, async () => {
+  try {
+    await dbconnection;
+    console.log("connected to DB");
+  } catch (error) {
+    console.log("failed to connect DB");
+    console.log({ error: error.message });
+  }
+  console.log(`server running at http://localhost:${port}`);
+});
