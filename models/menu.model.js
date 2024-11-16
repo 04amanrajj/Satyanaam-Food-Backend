991df19e1ff1b28fd36da6fc1736c127f74acbf0
04amanrajj <04.aman.raj@gmail.com>
2024-11-16 17:24:47 +0530

    feat(model): new modeles added

---
+++ models/menu.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const menuSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  image: { type: String, required: true },
+});
+
+exports.MenuModel = mongoose.model("menu-item", menuSchema);
---
+++ models/order.model.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const orderSchema = mongoose.Schema({
+  userID: { type: String, required: true },
+  items: { type: Array, required: true },
+  totalPrice: { type: Number, required: true },
+  status: { type: String, required: true },
+});
+
+exports.OrderModel = mongoose.model("order", orderSchema);
