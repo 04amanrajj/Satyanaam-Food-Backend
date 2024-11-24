74dc7fcc42b7f64bcfe56a89555f0ef1c28a5aac
04amanrajj <04.aman.raj@gmail.com>
2024-11-24 23:58:56 +0530

    Updated menu

---
+++ resources/modifyMenu.js
@@ -0,0 +1,15 @@
+// let data = require("./menu.json");
+
+// for (let category in data) {
+//   let category_data = data[category];
+
+//   for (let item of category_data) {
+//     item.rating = Number("4." + Math.floor(Math.random(2) * 10));
+//     item.price += item.price * (20 / 100);
+//   }
+// }
+
+// console.log(data);
+// const fs = require("fs");
+
+// fs.writeFileSync("updatedMenu.json", JSON.stringify(data, null, 2));
