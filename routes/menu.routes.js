930c6c48be29f699873df188e4853562aab490f6
04amanrajj <04.aman.raj@gmail.com>
2024-11-24 23:58:35 +0530

    refactor: functions moved to admin control

--- routes/menu.routes.js
+++ routes/menu.routes.js
@@ -1,28 +1,10 @@
 const express = require("express");
 const Router = require("express");
-const {
-  resetMenu,
-  getMenuItem,
-  deleteMenuItem,
-  updateMenuItem,
-  addMenuItem,
-} = require("../controllers/menu.controller");
-const { authenticate } = require("../middlewares/authorization.middleware");
-const { checkBlacklist } = require("../middlewares/checkBlacklist.middleware");
+const { getMenuItem } = require("../controllers/menu.controller");
 
 const menuRoutes = Router();
 
 // public route
 menuRoutes.get("/", getMenuItem);
-
-// middleware for protected routes
-menuRoutes.use(authenticate);
-menuRoutes.use(checkBlacklist);
-
-// admin-only routes
-menuRoutes.post("/reset", resetMenu); // reset menu
-menuRoutes.post("/", addMenuItem); // add a menu item
-menuRoutes.put("/:id", updateMenuItem); // update a menu item
-menuRoutes.delete("/:id", deleteMenuItem); // delete a menu item
 
 module.exports = { menuRoutes };
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,19 +1,12 @@
 const express = require("express");
 const Router = require("express");
 
-const {
-  getOrder,
-  newOrder,
-  updateOrder,
-} = require("../controllers/order.controller");
+const { getOrder, newOrder } = require("../controllers/order.controller");
 
 const orderRoute = Router();
 orderRoute.use(express.json());
 
 orderRoute.get("/", getOrder);
-
 orderRoute.post("/", newOrder);
-
-orderRoute.put("/:id", updateOrder);
 
 module.exports = { orderRoute };
