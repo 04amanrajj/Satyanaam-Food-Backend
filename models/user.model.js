d6a92d8a44a1d0930c35baa59836e35f9b5dc820
04amanrajj <04.aman.raj@gmail.com>
2024-11-24 23:58:25 +0530

    fix(model) changes added

--- models/user.model.js
+++ models/user.model.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  role: { type: String, required: true },
-  wishlist: Array,
-},{ versionKey: false });
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    phone: { type: Number, required: true },
+    role: { type: String, required: true },
+    wishlist: Array,
+  },
+  { versionKey: false }
+);
 
 exports.UserModel = mongoose.model("user", userSchema);
