959aed298d4617a9ddc9228718b3ae5e10f1fc67
04amanrajj <04.aman.raj@gmail.com>
2024-12-17 00:13:34 +0530

    remove: not useful logger

--- routes/default.routes.js
+++ routes/default.routes.js
@@ -9,10 +9,9 @@ defaultRoute.use(express.json());
 
 defaultRoute.get("/", async (req, res) => {
   try {
-    logger.info(`${req.role || "Some one"} visited.`);
     res.send({ data: information });
   } catch (error) {
-    logger.error(`Error showing menu: ${error.message}`);
+    logger.error(`Error showing info: ${error.message}`);
     console.log(error.message);
     res.status(500).send({ message: error.message });
   }
