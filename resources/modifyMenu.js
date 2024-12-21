c2ae32f4978494a1f7bb84b2320f6c9da84f17c6
04amanrajj <04.aman.raj@gmail.com>
2024-12-21 13:13:26 +0530

    update(menu): price changes

--- resources/modifyMenu.js
+++ resources/modifyMenu.js
@@ -1,11 +1,17 @@
 // let data = require("./menu.json");
-
+// const price = [
+//   150, 180, 200, 260, 40, 50, 50, 60, 15, 25, 120, 130, 150, 210, 140, 180, 20,
+//   180, 190, 170, 190, 170, 190, 130, 130, 120, 120, 120, 220, 180, 160, 180,
+//   180, 120, 120, 150, 180, 190, 180, 190, 80, 100, 120, 120, 120, 130, 120, 110,
+//   120, 120, 100, 10, 12, 15, 25, 15, 15, 20, 80, 90, 110, 120, 80, 90, 90, 10,
+//   30, 90,
+// ];
+// let i = 0;
 // for (let category in data) {
 //   let category_data = data[category];
 
 //   for (let item of category_data) {
-//     item.rating = Number("4." + Math.floor(Math.random(2) * 10));
-//     item.price += item.price * (20 / 100);
+//     item.price = price[i++];
 //   }
 // }
 
